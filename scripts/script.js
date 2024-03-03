9432ae792063ad53014dc8be8737f0f70766b8f5
Marcus Parker <marcus.parker@limbsandthings.com>
2024-03-03 16:50:56 +0000

    Webpage visual changes. JS file info dump

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// Start Game
+// Mole pops up for a random amount of time between TIME_MIN & TIME_MAX
+// Amount of moles that can pop up can be between MOLE_AMOUNT_MIN & MOLE_AMOUNT_MAX
+// Amount of moles gradually increase in time and amount as hit score goes up?
+// If mole is hit score is increased and mole retreats
+// If area is hit without mole hammer hp decreases
+// If hammer hp is zero game is over
+// If hit score is over a certain amount different moles start appearing
